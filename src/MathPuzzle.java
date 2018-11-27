4ffaad675719ef6ade153cafa69ed396c140b3da
Chan Lee <chanethanlee@gmail.com>
2018-11-26 22:18:03 -0800

    solve least common multiple.

--- src/MathPuzzle.java
+++ src/MathPuzzle.java
@@ -1,6 +1,6 @@
 public class MathPuzzle {
     public static void main(String[] args) {
-        System.out.println(EuclideanAlgorithm(10, 15));
+        System.out.println(LeastCommonMultiple(10, 15));
     }
 
     public static int GreatestCommonDivisor(int a, int b) {
@@ -15,11 +15,26 @@ public class MathPuzzle {
         return gcd;
     }
 
-    public static int EuclideanAlgorithm (int a, int b) {
+    public static int EuclideanAlgorithmRecursive (int a, int b) {
         if (b==0) {
             return a;
         } else {
-            return EuclideanAlgorithm(b, a%b);
+            return EuclideanAlgorithmRecursive(b, a%b);
         }
+    }
+
+    public static int EuclideanAlgorithm (int a, int b) {
+        while(b!=0) {
+            int r = a%b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+    //2609
+    // Cautious about the data size, as lcm is bigger than two numbers
+    public static int LeastCommonMultiple(int a, int b) {
+        // Using LG=AB equation
+        return (a*b)/EuclideanAlgorithm(a,b);
     }
 }
