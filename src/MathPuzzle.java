9d53edce703da1f3719e9525084983c1b2b2161a
Chan Lee <chanethanlee@gmail.com>
2018-11-26 22:52:27 -0800

    sovle base conversion problem in non-recursive/recursive way.

--- src/MathPuzzle.java
+++ src/MathPuzzle.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class MathPuzzle {
     public static void main(String[] args) {
-        System.out.println(LeastCommonMultiple(10, 15));
+//        System.out.println(BaseConversion());
+        System.out.println(BaseConversionRecursive());
     }
+
 
     public static int GreatestCommonDivisor(int a, int b) {
 
@@ -36,5 +40,48 @@ public class MathPuzzle {
     public static int LeastCommonMultiple(int a, int b) {
         // Using LG=AB equation
         return (a*b)/EuclideanAlgorithm(a,b);
+    }
+
+    //11005
+    //N is 10 base number. Convert it to B base number.(2<=B<=36)
+    // If 1 digit gets bigger than 9, use alphabet like
+    // A: 10, B:11, ... ,Z: 35
+    public static String BaseConversion() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int b = sc.nextInt();
+
+        String answer="";
+
+        while(n > 0) {
+            if(n%b < 10) {
+                answer = (n%b) + answer;
+                n = n/b;
+            } else {
+                int temp = (n%b) + 55;
+                answer =  (char) temp + answer;
+                n = n/b;
+            }
+        }
+        return answer;
+    }
+
+    public static String BaseConversionRecursive() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int b = sc.nextInt();
+        String answer ="";
+        return BaseConversionRecursive(n,b,answer);
+    }
+
+    public static String BaseConversionRecursive(int n, int b, String answer) {
+        if (n == 0 ) return answer;
+        if (n%b <10) {
+            answer = (n%b) + answer;
+        } else {
+            int temp = n%b + 55;
+            answer = (char) temp + answer;
+        }
+        return BaseConversionRecursive(n/b,b,answer);
     }
 }
